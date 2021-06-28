bea5711119bab33f7d55e316457b8481361c7e40
mirout <misharuz25@gmail.com>
2021-06-28 19:48:12 +0300

    Add location

---
+++ src/location.rs
@@ -0,0 +1,34 @@
+pub enum Location<'a> {
+    CityName {
+        name: &'a str,
+        state_code: Option<&'a str>,
+        country_code: Option<&'a str>,
+    },
+    CityId {
+        id: &'a str
+    },
+    Coordinates {
+        lat: f64,
+        lon: f64
+    },
+    ZipCode {
+        zip_code: &'a str,
+        country_code: Option<&'a str>
+    },
+    BoundingBox {
+        lon_left: f64,
+        lat_bottom: f64,
+        lon_right: f64,
+        lat_top: f64,
+        zoom: f64
+    },
+    Circle {
+        lat: f64,
+        lon: f64,
+        count: u32
+    },
+}
+
+impl Location {
+
+}
