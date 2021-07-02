2adcfb1dc3d3b85089fd2fc7cf4f81a1358ee06e
MIkhail Ruzavin <misharuz25@gmail.com>
2021-07-02 05:46:05 -0400

    Add pub and fix typo

--- src/weather.rs
+++ src/weather.rs
@@ -1,62 +1,64 @@
 use serde::{Serialize, Deserialize};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Coordinates {
-    lon: f32,
-    lat: f32,
+    pub lon: f32,
+    pub lat: f32,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Weather {
-    id: u32,
-    main: String,
-    description: String,
-    icon: String,
+    pub id: u32,
+    pub main: String,
+    pub description: String,
+    pub icon: String,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Main {
-    temp: f32,
-    pressure: f32,
-    humidity: f32,
-    temp_min: f32,
-    temp_max: f32,
+    pub temp: f32,
+    pub feels_like: f32,
+    pub temp_min: f32,
+    pub temp_max: f32,
+    pub pressure: f32,
+    pub humidity: f32,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Wind {
-    speed: f32,
-    deq: f32,
+    pub speed: f32,
+    pub deg: f32,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Clouds {
-    all: f32,
+    pub all: f32,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Sys {
     #[serde(rename="type")]
-    sys_type: u32,
+    pub sys_type: u32,
     pub id: u32,
-    message: f32,
-    country: String,
-    sunrise: u64,
-    sunset: u64,
+    pub country: String,
+    pub sunrise: u64,
+    pub sunset: u64,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct WeatherCurrent {
-    coord: Coordinates,
-    weather: Vec<Weather>,
-    base: String,
-    main: Main,
-    visibility: u32,
-    wind: Wind,
-    clouds: Clouds,
-    dt: u64,
+    pub coord: Coordinates,
+    pub weather: Vec<Weather>,
+    pub base: String,
+    pub main: Main,
+    pub visibility: u32,
+    pub wind: Wind,
+    pub clouds: Clouds,
+    pub dt: u64,
     pub sys: Sys,
-    id: u32,
+    pub timezone: u32,
+    pub id: u64,
     pub name: String,
-    cod: u32,
+    pub cod: u32,
 }
+// {"coord":{"lon":-0.1257,"lat":51.5085},"weather":[{"id":803,"main":"Clouds","description":"broken clouds","icon":"04d"}],"base":"stations","main":{"temp":291.77,"feels_like":291.75,"temp_min":288.76,"temp_max":294.11,"pressure":1018,"humidity":79},"visibility":9000,"wind":{"speed":2.57,"deg":260},"clouds":{"all":75},"dt":1625218116,"sys":{"type":2,"id":268730,"country":"GB","sunrise":1625197696,"sunset":1625257232},"timezone":3600,"id":2643743,"name":"London","cod":200}
