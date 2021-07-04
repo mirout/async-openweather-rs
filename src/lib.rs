6cd0d08ee319c0355892c19a134c4dfde594d928
Mikhail Ruzavin <misharuz25@gmail.com>
2021-07-04 20:18:10 +0300

    Add multi location test

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ pub mod weather;
 
 #[cfg(test)]
 mod tests {
-    use crate::location::UnitLocation;
+    use crate::location::{MultiLocation, UnitLocation};
     use crate::network::{CurrentWeather, OpenWeatherClient};
     use std::env;
 
@@ -56,5 +56,41 @@ mod tests {
         };
         let result = client.get_current_weather(city).await.unwrap();
         assert_eq!(result.name, "New York")
+    }
+
+    #[tokio::test]
+    async fn test_current_weather_bbox() {
+        let token = env::var("OWT").unwrap();
+        let client = OpenWeatherClient::new(token);
+        let cities = MultiLocation::BoundingBox {
+            lon_left: 12.0,
+            lat_bottom: 32.0,
+            lon_right: 15.0,
+            lat_top: 37.0,
+            zoom: 10.0,
+        };
+        let result = client.get_current_weather(cities).await.unwrap();
+        assert!(result
+            .list
+            .into_iter()
+            .find(|x| x.name == "Yafran")
+            .is_some())
+    }
+
+    #[tokio::test]
+    async fn test_current_weather_circle() {
+        let token = env::var("OWT").unwrap();
+        let client = OpenWeatherClient::new(token);
+        let cities = MultiLocation::Circle {
+            lat: 55.751244,
+            lon: 37.618423,
+            count: 3,
+        };
+        let result = client.get_current_weather(cities).await.unwrap();
+        assert!(result
+            .list
+            .into_iter()
+            .find(|x| x.name == "Moscow")
+            .is_some())
     }
 }
